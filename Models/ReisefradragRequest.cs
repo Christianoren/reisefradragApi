e632ecc0443d478b1ba4eccce7c845c4fc9e3c99
Christian Øren <Christian.oren@hotmail.no>
2021-03-16 18:41:51 +0100

    added reisefradrag model

---
+++ Models/ReisefradragRequest.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace reisefradragApi.Models
+{
+    public class ReisefradragRequest
+    {
+        public List<Arbeidsreise> Arbeidsreiser { get; set; }
+        public List<Besoeksreise> Besoeksreiser { get; set; }
+        public int UtgifterBomFergeEtc { get; set; }
+        
+        public class Arbeidsreise
+        {
+        public int Km { get; set; }
+        public int Antall { get; set; }
+        }
+
+        public class Besoeksreise
+        {
+        public int Km { get; set; }
+        public int Antall { get; set; }
+        }
+    }
+}
